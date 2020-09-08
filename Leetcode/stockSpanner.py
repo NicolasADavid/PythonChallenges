7ce24659ed2eaf3be0e5e915da07624157a6d091
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-09-08 19:53:23 -0400

    stockSpanner

---
+++ Leetcode/stockSpanner.py
@@ -0,0 +1,14 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+        # (price, weight)
+
+    def next(self, price: int) -> int:
+        weight = 1
+        while self.stack and self.stack[-1][0] <= price:
+            weight += self.stack.pop()[1]
+
+        self.stack.append((price, weight))
+
+        return weight
