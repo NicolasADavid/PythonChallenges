c83c7bc26e0a5635eb9df784ad92e1065a0395a4
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-09-09 22:42:28 -0400

    testing

--- Leetcode/stockSpanner.py
+++ Leetcode/stockSpanner.py
@@ -12,3 +12,12 @@ class StockSpanner:
         self.stack.append((price, weight))
 
         return weight
+
+
+if __name__ == "__main__":
+    s = StockSpanner()
+
+    input = [11, 3, 9, 5, 6, 4, 7]
+    
+    for inp in input:
+        print(s.next(inp))
