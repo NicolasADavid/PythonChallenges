57bf29aef7d0223c482bf15f9c73bac5790c453d
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-09-13 20:58:26 -0400

    word-search backtracking

--- Code Club/word-search.py
+++ Code Club/word-search.py
@@ -2,7 +2,8 @@ from typing import List
 
 class Solution:
 
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    # without backtracking
+    def exist2(self, board: List[List[str]], word: str) -> bool:
 
         if not board:
             return False
@@ -47,6 +48,47 @@ class Solution:
                     return True
 
         return False
+
+    # backtracking
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+        self.board = board
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, word):
+                    return True
+
+        return False
+
+    def backtrack(self, row, col, suffix):
+        #bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # check current status
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS or self.board[row][col] != suffix[0]:
+            return False
+
+        ret = False
+
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [ (0, 1), (1, 0), (-1, 0), (0, -1) ]:
+            #remove first letter of suffix to continue the search
+            ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
+
+            # break instead of return directly to cleanup the marked choices
+            if ret: break
+
+        # revert the change
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, did not find match  
+        return ret
 
 if __name__ == "__main__":
     s = Solution()
