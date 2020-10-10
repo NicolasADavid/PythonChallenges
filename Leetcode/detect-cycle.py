d8e59ad1e30d264c13feca090aa87e276ca3b1b0
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-10-10 01:12:14 -0400

    detect-cycle

---
+++ Leetcode/detect-cycle.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+
+        seen = set()
+
+        node = head
+
+        while node is not None:
+            if node in seen:
+                return node
+
+            seen.add(node)
+            node = node.next
+
+        return None
+            
