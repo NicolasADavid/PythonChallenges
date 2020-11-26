3ec811ccdb9b54c8c1566d59e4e1db0b833a3589
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-11-26 10:22:23 -0500

    subsets-two

--- Leetcode/subsets-two.py
+++ Leetcode/subsets-two.py
@@ -2,20 +2,39 @@ from typing import List
 
 class Solution:
     
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
 
         # start with an empty list in the output
         output = [[]]
+        seen = set()
         
         # for each num, add num to every element in output
+        # O(n)t
         for num in nums:
+
             # add to output
             for curr in output.copy():
+
+                #O(n)t O(n)s
                 new = curr.copy() + [num]
+
+                #O(nlgn)t -> n^2 ??
+                new.sort()
+                
+                #O(n)t
+                t = tuple(new)
+
+                #O(1)t
+                if t in seen:
+                    continue
+
+                #O(1)t
+                seen.add(t)
+
                 output += [new]
         
         return output
         
 s = Solution()
-print(s.subsets([1,2,3]))
+print(s.subsetsWithDup([1,2,3,3]))
