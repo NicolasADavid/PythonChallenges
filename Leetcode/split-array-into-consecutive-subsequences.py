29bf5ff6871ea3be3e46a5429388c85fef27ca6c
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-11-26 12:03:15 -0500

    split-array-into-consecutive-subsequences

--- Leetcode/split-array-into-consecutive-subsequences.py
+++ Leetcode/split-array-into-consecutive-subsequences.py
@@ -7,9 +7,12 @@ class Solution:
 
     def isPossible(self, nums: List[int]) -> bool:
 
+        # O(n)s, potentially one list for every input num
         map = defaultdict(list)
         tb = 0 # tiebreaker
 
+        # O(n)t
+        
         for num in nums:
 
             # If len(map[num]), pop target from map[num], insert num to target, append to map[num+1]
@@ -24,15 +27,18 @@ class Solution:
                 heapq.heappush(map[num+1], nt)
             else:
                 # pop target of min length from TargetSet
+                # O(n) * O(lgn) t
                 t = heapq.heappop(map[num])
                 nt = (
                     t[0] + 1,               # inc size
                     t[1],                   # tiebreaker
                     t[2] + [num]            # add num
                 )
+                # O(n) * O(lgn) t
                 heapq.heappush(map[num+1], nt)
 
         # Check accumulated target lists' lengths
+        # O(n)t
         for targetSet in map.values():
             for target in targetSet:
                 if target[0] < 3:
