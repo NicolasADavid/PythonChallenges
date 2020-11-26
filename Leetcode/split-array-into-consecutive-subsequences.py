9b1e9d26aec9e9e315ce372524e2ae66bdbf9f45
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-11-26 11:55:17 -0500

    split-array-consecutive-subsequences

---
+++ Leetcode/split-array-into-consecutive-subsequences.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/split-array-into-consecutive-subsequences/
+from typing import List
+from collections import defaultdict
+import heapq
+
+class Solution:
+
+    def isPossible(self, nums: List[int]) -> bool:
+
+        map = defaultdict(list)
+        tb = 0 # tiebreaker
+
+        for num in nums:
+
+            # If len(map[num]), pop target from map[num], insert num to target, append to map[num+1]
+            # If not len(map[num]), create new list, append to map[num+1]
+            # Maintain map target lists sorted by length of targets. Want to insert nums into shortest lists.
+
+            if not len(map[num]):
+                # TargetSet is empty
+                # insert new target to TargetSet of map[num+1].
+                nt = (1, tb, [num])
+                tb += 1
+                heapq.heappush(map[num+1], nt)
+            else:
+                # pop target of min length from TargetSet
+                t = heapq.heappop(map[num])
+                nt = (
+                    t[0] + 1,               # inc size
+                    t[1],                   # tiebreaker
+                    t[2] + [num]            # add num
+                )
+                heapq.heappush(map[num+1], nt)
+
+        # Check accumulated target lists' lengths
+        for targetSet in map.values():
+            for target in targetSet:
+                if target[0] < 3:
+                    return False
+
+        return True
+
+if __name__ == "__main__":
+    s = Solution()
+    s.isPossible([1,2,3,3,4,5]) # true
