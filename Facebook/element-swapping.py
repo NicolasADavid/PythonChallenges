e5421795d6c7d942f5ba50d5c5c8e062a9ffec7c
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-12-08 22:32:26 -0500

    element-swapping

--- Facebook/element-swapping.py
+++ Facebook/element-swapping.py
@@ -22,26 +22,42 @@
 # output = [2, 8, 9, 11, 1]
 # We can swap [11, 2], followed by [9, 2], then [8, 2].
 
-
 import math
-# Add any extra import statements you may need here
-
-
-# Add any helper functions you may need here
-
 
 def findMinArray(arr, k):
-  # Write your code here
+  left = 0
   
-	
+  minIdx = left
+  
+  while k > 0 and left < len(arr):
+  
+    for i in range(left, min(left + k + 1, len(arr))):
+      # find minimum element within k steps from left pointer
 
+      # Found an element smaller than element at minIdx
+      if arr[i] < arr[minIdx]:
+        minIdx = i
+  
+    # move min element up. Min can move 0 steps.
+    moves = minIdx - left
+    
+    # How to move an element up correctly?
+    # Bubble forward element at minIdx
+    for i in range(moves):
+      moveFrom = minIdx - 1
+      moveBack = arr[moveFrom]
+      
+      arr[moveFrom] = arr[minIdx]
+      arr[minIdx] = moveBack
+      minIdx -= 1
+    
+    # Decrement k by moves used
+    # Increment left
+    k -= moves
+    left += 1
+    minIdx = left
 
-
-
-
-
-
-
+  return arr
 
 # These are the tests we use to determine if the solution is correct.
 # You can add your own at the bottom, but they are otherwise not editable!
@@ -95,6 +111,48 @@ if __name__ == "__main__":
   expected_2 = [2, 8, 9, 11, 1]
   output_2 = findMinArray(arr_2,k_2)
   check(expected_2, output_2)
+    
+  n_3 = 5
+  arr_3 = [5,4,3,2,1]
+  k_3 = 10
+  expected_3 = [1,2,3,4,5]
+  output_3 = findMinArray(arr_3,k_3)
+  check(expected_3, output_3)
+    
+  n_4 = 5
+  arr_4 = [5,4,3,2,1]
+  k_4 = 11
+  expected_4 = [1,2,3,4,5]
+  output_4 = findMinArray(arr_4,k_4)
+  check(expected_4, output_4)
+    
+  n_5 = 5
+  arr_5 = [1,1,1,1,1]
+  k_5 = 11
+  expected_5 = [1,1,1,1,1]
+  output_5 = findMinArray(arr_5,k_5)
+  check(expected_5, output_5)
+    
+  n_6 = 1
+  arr_6 = [1]
+  k_6 = 100
+  expected_6 = [1]
+  output_6 = findMinArray(arr_6,k_6)
+  check(expected_6, output_6)
+    
+  n_7 = 5
+  arr_7 = [1,2,1,2,1]
+  k_7 = 3
+  expected_7 = [1,1,1,2,2]
+  output_7 = findMinArray(arr_7,k_7)
+  check(expected_7, output_7)
+    
+  n_8 = 5
+  arr_8 = [1,2,1,2,1]
+  k_8 = 2
+  expected_8 = [1,1,2,1,2]
+  output_8 = findMinArray(arr_8,k_8)
+  check(expected_8, output_8)
 
   # Add your own test cases here
   
