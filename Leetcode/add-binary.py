573c517e5e1c01f540bd41df19e6cbb14bf1322a
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-12-15 23:53:01 -0500

    add-binary

---
+++ Leetcode/add-binary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a, b = list(a), list(b)
+        carry = 0
+        ans = ""
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            carry, remain = divmod(carry, 2)
+            ans += str(remain)
+        return ans[::-1]
+
+
+if __name__ == "__main__":
+    print(Solution().addBinary("11","1"))
