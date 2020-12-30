33ed09297a46104cc0f88d179c28ca1e9ca5e669
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2020-12-30 15:23:29 -0500

    decode-ways

--- Leetcode/decodeWays.py
+++ Leetcode/decodeWays.py
@@ -26,28 +26,6 @@ class Solution:
 
         return ans
 
-    def canTakeOne(self, input) -> bool:
-        if not input:
-            return 0
-
-        if input[0] not in [0]:
-            return True
-
-    def canTakeTwo(self, input) -> bool:
-        if not input:
-            return False
-
-        if len(input) < 2:
-            return False
-
-        if input[0] == '1':
-            return True
-
-        if input[0] == '2':
-            if input[1] not in ['7', '8', '9']:
-                return True
-        
-        return False
                 
 if __name__ == "__main__":
     s = Solution()
