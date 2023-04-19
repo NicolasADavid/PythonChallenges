2abb0ec6f22d2bb28b34767536f8958fdf249f73
nicolasadavid <4590309+NicolasADavid@users.noreply.github.com>
2023-04-19 15:26:52 -0700

    contains-duplicate.py

---
+++ Leetcode/contains-duplicate.py
@@ -0,0 +1,12 @@
+# from collections import set
+from typing import List, Set
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
