9663d123a1aa55383ce1d6910e6701bd67217a70
nicolasadavid <4590309+NicolasADavid@users.noreply.github.com>
2023-04-25 04:14:05 -0700

    spiral-matrix

--- Leetcode/spiral-matrix.py
+++ Leetcode/spiral-matrix.py
@@ -2,6 +2,52 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+      if not matrix:
+        return []
+      
+      h = len(matrix)
+      w = len(matrix[0])
+      n = h * w
+      ans = []
+
+      fromtop = 0
+      fromright = 0
+      frombottom = 0
+      fromleft = 0
+      
+      while n > 0:
+        #left to right
+        for i in range(fromleft, w - fromright):
+          ans.append(matrix[fromtop][i])
+          n = n-1
+          if n == 0: return ans
+        fromtop = fromtop + 1
+          
+        #top to bottom
+        for i in range(fromtop, h - frombottom):
+           ans.append(matrix[i][w - 1 - fromright])
+           n = n-1
+           if n == 0: return ans
+        fromright = fromright + 1
+
+        #right to left
+        for i in reversed(range(fromleft, w - fromright)):
+           ans.append(matrix[h - 1 - frombottom][i])
+           n = n-1
+           if n == 0: return ans
+        frombottom = frombottom + 1
+
+        #bottom to top
+        for i in reversed(range(fromtop, h - frombottom)):
+           ans.append(matrix[i][fromleft])
+           n = n-1
+           if n == 0: return ans
+        fromleft = fromleft + 1
+
+      return ans
+       
+
+    def spiralOrder1(self, matrix: List[List[int]]) -> List[int]:
 
         if not matrix: return []
     
@@ -49,10 +95,10 @@ class Solution:
         return res
 
 
-if __name__ == "__main__":
-    s = Solution()
-    r = s.spiralOrder(matrix = [[1,2,3],[4,5,6],[7,8,9]])
-    print(r)
+# if __name__ == "__main__":
+#     s = Solution()
+#     r = s.spiralOrder(matrix = [[1,2,3],[4,5,6],[7,8,9]])
+#     print(r)
 
 def printString(string):
   print('[\"', string, '\"]', sep='', end='')
@@ -83,4 +129,9 @@ if __name__ == "__main__":
     expected_1 = [1,2,3,6,9,8,7,4,5]
     output_1 = s.spiralOrder(input_1)
     check(expected_1, output_1)
+
+    input_2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    expected_2 = [1,2,3,4,8,12,11,10,9,5,6,7]
+    output_2 = s.spiralOrder(input_2)
+    check(expected_2, output_2)
   
