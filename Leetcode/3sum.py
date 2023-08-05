57f6c251b1af29a6af426c69806059a958c70c2d
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2023-08-05 16:33:19 -0700

    3sum

--- Leetcode/3sum.py
+++ Leetcode/3sum.py
@@ -38,6 +38,35 @@ class Solution:
 
 
         return threeSolsSet
+    
+    def threeSumII(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+
+            if i == 0 or nums[i] != nums[i-1]:
+                self.twoSumII(nums, i, res)
+
+        return res
+    
+    def twoSumII(self, nums: List[int], i: int, res: List):
+        lo, hi = i + 1, len(nums)-1
+        while lo < hi:
+            sum = nums[i] + nums[lo] + nums[hi]
+            if sum < 0:
+                lo += 1
+            elif sum > 0:
+                hi -= 1
+            else:
+                res.append((nums[i], nums[lo], nums[hi]))
+                lo += 1
+                hi -= 1
+                while lo < hi and nums[lo] == nums[lo - 1]:
+                    lo += 1
+    
         
 if __name__ == "__main__":
     solution = Solution()
@@ -45,11 +74,7 @@ if __name__ == "__main__":
     i1 = [-1, 0, 1, 2, -1, 4]
     
     print(solution.threeSum(i1))
-
-
-
-
-
+    print(solution.threeSumII(i1))
 
 
 # class Solution:
