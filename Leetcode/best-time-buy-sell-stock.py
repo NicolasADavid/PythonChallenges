5bc82d7af9b82ca5c2879bd10610080c52695fff
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2023-08-07 18:19:45 -0700

    best-time-buy-sell-stock

--- Leetcode/best-time-buy-sell-stock.py
+++ Leetcode/best-time-buy-sell-stock.py
@@ -14,12 +14,25 @@ class Solution:
             if profit(nextBuyAt, idx) > profit(buyAt, sellAt):
                 buyAt = nextBuyAt
                 sellAt = idx
-            if price < prices[buyAt]:
+            if price < prices[nextBuyAt]:
                 nextBuyAt = idx
             
         return profit(buyAt, sellAt)
+
+class Solution2:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+                
+        return max_profit
 
 if __name__ == "__main__":
     s = Solution()
     print(s.maxProfit([7,1,5,3,6,4])) # 5
     print(s.maxProfit([7,6,4,3,1])) # 0
+    print(s.maxProfit([4,7,1,2,11])) # 10
