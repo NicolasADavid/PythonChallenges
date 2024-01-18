c0222633d882006795377c0a8cec12568a362fb1
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2024-01-17 16:15:19 -0800

    merge-two-sorted-lists

--- Leetcode/merge-two-sorted-lists.py
+++ Leetcode/merge-two-sorted-lists.py
@@ -1,6 +1,5 @@
 
 from typing import Optional
-from collections import deque
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -9,27 +8,25 @@ class ListNode:
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        head = ListNode(next = None)
-        last = head
+        last = head = ListNode()
 
         while list1 and list2:
             if list1.val <= list2.val:
                 last.next = ListNode(list1.val)
-                last = last.next
                 list1 = list1.next
             else:
                 last.next = ListNode(list2.val)
-                last = last.next
                 list2 = list2.next
+            last = last.next
         
         while list1:
-                last.next = ListNode(list1.val)
-                last = last.next
-                list1 = list1.next
+            last.next = ListNode(list1.val)
+            last = last.next
+            list1 = list1.next
         while list2:
-                last.next = ListNode(list2.val)
-                last = last.next
-                list2 = list2.next
+            last.next = ListNode(list2.val)
+            last = last.next
+            list2 = list2.next
 
         return head.next
     
@@ -37,3 +34,7 @@ s = Solution()
 l1 = ListNode(val = 1, next = ListNode(val = 2, next = ListNode(val = 4, next = None)))
 l2 = ListNode(val = 1, next = ListNode(val = 3, next = ListNode(val = 4, next = None)))
 r1 = s.mergeTwoLists(l1, l2)
+
+while r1:
+    print(r1.val)
+    r1 = r1.next
