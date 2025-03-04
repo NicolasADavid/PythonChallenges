64c367c1f312adc36946a19cb5169107690c6e29
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2025-03-03 18:02:39 -0800

    script

---
+++ Leetcode/script.py
@@ -0,0 +1,22 @@
+
+x = 4 // 2
+y = 5 // 2
+
+print("x: ", x)
+print("y: ", y)
+
+
+x = 4 / 2
+y = 5 / 2
+
+print("x: ", x)
+print("y: ", y)
+
+string = "The quick red fox jumped over the lazy brown dog."
+idx = string.find("quick")
+
+
+
+
+
+
