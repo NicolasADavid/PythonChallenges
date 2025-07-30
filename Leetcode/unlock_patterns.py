1f5237930961d876b723870fc9e28207ef419eb6
NicolasADavid <4590309+NicolasADavid@users.noreply.github.com>
2025-07-29 17:50:53 -0700

    unlock_patterns

--- Leetcode/unlock_patterns.py
+++ Leetcode/unlock_patterns.py
@@ -15,17 +15,28 @@ A pattern is valid if the following criteria are met:
 NOTE: It is possible to go from 2 to 9 (or 3 to 4) because the pattern may move between rows and colunns on diagonals.
 
 
-Algorithm:
+Android devices have a special lock screen with a 3 x 3 grid of dots. Users can set an "unlock pattern" by connecting 
+the dots in a specific sequence, forming a series of joined line segments where each segment's endpoints are two 
+consecutive dots in the sequence. A sequence of k dots is a valid unlock pattern if both of the following are true:
 
+All the dots in the sequence are distinct.
+If the line segment connecting two consecutive dots in the sequence passes through the center of any other dot, 
+the other dot must have previously appeared in the sequence. No jumps through the center non-selected dots are allowed.
+For example, connecting dots 2 and 9 without dots 5 or 6 appearing beforehand is valid because the line 
+from dot 2 to dot 9 does not pass through the center of either dot 5 or 6.
 
-Create a jump matrix that indicates which numbers are jumped over when moving from one number to another.
+However, connecting dots 1 and 3 without dot 2 appearing beforehand is invalid because the line from dot 1 to dot 3 passes through the center of dot 2.
 
+Here are some example valid and invalid unlock patterns:
 
-Start from every number.
+The 1st pattern [4,1,3,6] is invalid because the line connecting dots 1 and 3 pass through dot 2, but dot 2 did not previously appear in the sequence.
+The 2nd pattern [4,1,9,2] is invalid because the line connecting dots 1 and 9 pass through dot 5, but dot 5 did not previously appear in the sequence.
+The 3rd pattern [2,4,1,3,6] is valid because it follows the conditions. The line connecting dots 1 and 3 meets the condition because dot 2 previously appeared in the sequence.
+The 4th pattern [6,5,4,1,9,2] is valid because it follows the conditions. The line connecting dots 1 and 9 meets the condition because dot 5 previously appeared in the sequence.
 
-If path is of length N, return 1
-Consider every possible next number that is not already visted and does not jump over an unused number
-Explore that path, incrementing the path length by 1
+Given two integers m and n, return the number of unique and valid unlock patterns of the Android grid lock screen that consist of at least m keys and at most n keys.
+
+Two unlock patterns are considered unique if there is a dot in one sequence that is not in the other, or the order of the dots is different.
 
 """
 
@@ -98,20 +109,21 @@ class Solution(object):
 
         # 1, 3, 7, 9 (corners) are symmetric. Only need to calculate possibilities from one of them
         # 2, 4, 6, 8 (edges) are symmetric.
-        # 5 is the center and can be used in any pattern.
+        # 5 (center) is unique
 
         corner = helper(1)
         edge = helper(2)
         center = helper(5)
 
         total += 4 * corner + 4 * edge + center
+
         return total
 
 if __name__ == "__main__":
+
     assert Solution().numberOfPatterns(m = 1, n = 1) == 9
     assert Solution().numberOfPatterns(m = 1, n = 2) == 65
     assert Solution().numberOfPatterns(m = 1, n = 3) == 385
-
 
     assert Solution().numberOfPatterns(1, 1) == 9
     assert Solution().numberOfPatterns(2, 2) == 56
